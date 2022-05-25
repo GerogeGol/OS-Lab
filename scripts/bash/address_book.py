619022d0380a87d4f5b944c62767e99fb220ddd0
George Golubev <george.gol003@gmail.com>
2022-05-25 11:12:53 +0300

    add

---
+++ scripts/bash/address_book.py
@@ -0,0 +1,56 @@
+import json
+import sys
+from dataclasses import dataclass
+
+from pyrsistent import field
+
+
+@dataclass
+class Contact:
+    name: str
+    surname: str
+    email: str
+    phone_number: str
+    info: str
+
+    def __str__(self):
+        return (
+            f"{self.name} {self.surname} {self.email} {self.phone_number} {self.info}"
+        )
+
+
+def create_user(fields: list[str]) -> Contact:
+    name = fields[0]
+    surname = fields[1]
+    email = fields[2]
+    phone_number = fields[3]
+    info = fields[4]
+    return Contact(name, surname, email, phone_number, info)
+
+
+def add_user(database: dict, contact: Contact) -> None:
+    ...
+
+
+def delete_user(database: dict, contact: Contact) -> None:
+    ...
+
+
+help_mess = "usage: address_book OPTION name surname email phone_number info\n-h help\n-a add\n -d delete\n -l list"
+
+
+def main():
+    match sys.argv:
+        case ["-a", *fields]:
+            ...
+        case ["-d", *fields]:
+            ...
+        case ["-l"]:
+            ...
+        case _:
+            print(help_mess)
+            exit(0)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/bash/dir_file_exist.py
+++ scripts/bash/dir_file_exist.py
@@ -3,9 +3,10 @@
 import sys
 import os
 import os.path
-PATH  = sys.argv[1]
-FIND = sys.argv[2]
 
-os.chdir(PATH)
-os.path.exists()
-print(PATH, FIND)
+PATH = sys.argv[1]
+TO_FIND = sys.argv[2]
+
+flag = os.path.exists(PATH + TO_FIND)
+
+if not flag:
